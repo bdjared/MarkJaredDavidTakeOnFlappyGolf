779b0189cc0eba9dc2f27b185753ef4899b71d2e
Jared Hynes <jarhyn22@bergen.org>
2019-05-30 12:34:21 -0400

    Trying to make a main screen.

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -1,2 +1,33 @@
-public class MainScreen {
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainScreen extends Application {
+    public void start(Stage primaryStage) {
+        BorderPane borderPane = new BorderPane();
+
+        borderPane.setTop(new CustomPane("Jared, David, and Mark Land"));
+
+        Button level1 = new Button("Level 1");
+        level1
+
+        Scene scene = new Scene(borderPane, 900, 600);
+        primaryStage.setTitle("Better Flappy Golf");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
+
+class CustomPane extends StackPane {
+    public CustomPane(String title) {
+        getChildren().add(new Label(title));
+        setStyle("-fx-border-color: red");
+        setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
+    }
 }
