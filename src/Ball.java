b5881f822275958f3936794bdff88fe8266cf4ea
davkri22 <davkri22@bergen.org>
2019-05-30 11:47:45 -0400

    yeet

---
+++ src/Ball.java
@@ -0,0 +1,13 @@
+import javafx.scene.shape.Circle;
+
+public class Ball extends Circle {
+    public double hSpeed;
+    public double ySpeed;
+
+    public Ball() {
+    }
+
+    public static void left() {
+
+    }
+}
