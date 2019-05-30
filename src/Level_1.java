2d914651410cfc267c8a6cd2750f2cbcfc97691c
Mark-Malysa <marmal22@bergen.org>
2019-05-30 12:34:42 -0400

    hi

--- src/Level_1.java
+++ src/Level_1.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
@@ -12,12 +13,15 @@ public class Level_1 extends Application {
         Ball player = new Ball(100, 100);
         p.getChildren().add(player);
 
-        Rectangle rS = new Rectangle();
+        Rectangle rS = new Rectangle(100,100);
+
         rS.xProperty().setValue(100);
         rS.yProperty().setValue(100);
+        rS.setStroke(Color.BLACK);
+        rS.setFill(Color.WHITE);
         p.getChildren().add(rS);
 
-        Scene scene = new Scene(p);
+        Scene scene = new Scene(p, 900,600);
         ps.setScene(scene);
         ps.show();
         ps.requestFocus();
