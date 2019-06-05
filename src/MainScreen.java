b6847d07cc216e39f1a6a8469f93acc4c631a098
davkri22 <davkri22@bergen.org>
2019-06-05 12:32:15 -0400

    yeet

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -17,22 +17,6 @@ public class MainScreen extends Application {
         borderPane.setTop(new CustomPane("Jared, David, and Mark Land"));
         borderPane.setCenter(pane);
         borderPane.setBackground(new Background(new BackgroundFill(Color.DEEPSKYBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
-//        gridPane.gridLinesVisibleProperty().setValue(Boolean.TRUE);
-
-//        Rectangle filler0 = new Rectangle(45, 45);
-//        filler0.setFill(Color.PALEGOLDENROD);
-//        filler0.setStroke(Color.PALEGOLDENROD);
-//        gridPane.add(filler0, 0, 2);
-
-//        Rectangle filler5 = new Rectangle(45, 45);
-//        filler5.setFill(Color.PALEGOLDENROD);
-//        filler5.setStroke(Color.PALEGOLDENROD);
-//        gridPane.add(filler5, 0, 3);
-
-//        Rectangle filler6 = new Rectangle(45, 45);
-//        filler6.setFill(Color.PALEGOLDENROD);
-//        filler6.setStroke(Color.PALEGOLDENROD);
-//        gridPane.add(filler6, 0, 4);
 
         Circle level1 = new Circle(35);
         level1.setCenterX(50);
@@ -69,56 +53,8 @@ public class MainScreen extends Application {
         Circle level9 = new Circle(35);
         level9.setCenterX(800);
         level9.setCenterY(300);
-//        level1.setShape(new Circle(350));
-//        level1.setMaxSize(350, 350);
-//        gridPane.add(level1, 1, 5);
-//
-//        Button level2 = new Button("Level 2");
-//        level2.setShape(new Circle(15));
-//        level2.setMaxSize(350,350);
-//        gridPane.add(level2, 3, 5);
-
-//        Rectangle filler2 = new Rectangle(45, 45);
-//        filler2.setFill(Color.PALEGOLDENROD);
-//        filler2.setStroke(Color.PALEGOLDENROD);
-//        gridPane.add(filler2, 2, 1);
-
-
-//        Rectangle filler1 = new Rectangle(1, 50);
-//        filler1.setFill(Color.PALEGOLDENROD);
-//        filler1.setStroke(Color.PALEGOLDENROD);
-//        gridPane.add(filler1, 0,0);
-//
-//        Button level3 = new Button("Level 3");
-//        level3.setShape(new Circle(350));
-//        level3.setMaxSize(350, 350);
-//        gridPane.add(level3, 4, 5);
-
-//        Rectangle filler3 = new Rectangle(45, 45);
-//        filler3.setFill(Color.PALEGOLDENROD);
-//        filler3.setStroke(Color.PALEGOLDENROD);
-//        gridPane.add(filler3, 5,1);
-
-//
-//        Button level4 = new Button("Level 4");
-//        level4.setShape(new Circle(350));
-//        level4.setMaxSize(350, 350);
-//        gridPane.add(level4, 6, 5);
-
-//        Rectangle filler4 = new Rectangle(45, 45);
-//        filler4.setFill(Color.PALEGOLDENROD);
-//        filler4.setStroke(Color.PALEGOLDENROD);
-//        gridPane.add(filler4, 7,1);
-
-
-//        Button level5 = new Button("Level 5");
-//        level5.setShape(new Circle(350));
-//        level5.setMaxSize(350, 350);
-//        level5.
-//        gridPane.add(level5, 8, 5);
 
         pane.getChildren().addAll(level1, level2, level3, level4, level5, level6, level7, level8, level9);
-
 
         Scene scene = new Scene(borderPane, 900, 600);
         primaryStage.setTitle("Better Flappy Golf");
@@ -146,8 +82,6 @@ public class MainScreen extends Application {
             primaryStage.requestFocus();
         });
 
-
-
         level2.setOnMousePressed(e -> {
             Level_2 lvl2 = new Level_2();
             Scene lvlScene = new Scene(lvl2, 900, 600);
@@ -173,7 +107,6 @@ public class MainScreen extends Application {
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
-
     }
 }
 
