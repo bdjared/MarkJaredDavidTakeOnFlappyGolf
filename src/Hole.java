5026a946ed0f2626e148f6bf4640dc8aef21e519
davkri22 <davkri22@bergen.org>
2019-06-05 12:09:07 -0400

    yeet

--- src/Hole.java
+++ src/Hole.java
@@ -1,6 +1,5 @@
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Ellipse;
-import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 
 public abstract class Hole extends Pane {
@@ -9,7 +8,7 @@ public abstract class Hole extends Pane {
 
     public abstract Rectangle getGreen();
 
-    public abstract Polygon getGrass();
+    public abstract Rectangle getGrass();
 
     public abstract Rectangle getHoleRect();
 }
